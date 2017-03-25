9b4a1a1eba2ae14204f67568c9cb66e1c9fc8b19
Jason C. Stone <jasoncstone@gmail.com>
2017-03-24 23:19:38 -0500

    Added support for {user} is typing

--- index.js
+++ index.js
@@ -10,11 +10,16 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
   console.log('a user connected');
   socket.broadcast.emit('connection status', socket.id + " connected!");
+
+  // Init new object and add to sockets collection //
   sockets[socket.id] = {};
   sockets[socket.id].socket = socket;
+  sockets[socket.id].typing = false;
 
   socket.on('chat message', function(msg){
-   socket.broadcast.emit('chat message', (sockets[socket.id].nick || socket.id) + ": " + msg); 
+    socket.broadcast.emit('chat message', (sockets[socket.id].nick || socket.id) + ": " + msg);
+    sockets[socket.id].typing = false;
+    socket.broadcast.emit('typing', socket.id, (sockets[socket.id].nick || socket.id), false);
   });
 
   socket.on('msg', function(id, msg){
@@ -23,17 +28,19 @@ io.on('connection', function(socket){
       for(props in sockets){
         if(sockets[props].nick == id){
           dest_socket = sockets[props].socket;
-        } 
+        }
       }
     }else{
     	dest_socket = sockets[id].socket;
     }
 
     if(dest_socket){
-      dest_socket.emit("chat message", "<div id='private'>Private message from " + (sockets[socket.id].nick || socket.id) + ": " + msg + "</div>"); 
+      dest_socket.emit("chat message", "<div id='private'>Private message from " + (sockets[socket.id].nick || socket.id) + ": " + msg + "</div>");
     }else{
-      socket.emit("chat message", "Error: " + id + " could not be found!"); 
+      socket.emit("chat message", "Error: " + id + " could not be found!");
     }
+    sockets[socket.id].typing = false;
+    socket.broadcast.emit('typing', socket.id, (sockets[socket.id].nick || socket.id), false);
 
   });
 
@@ -42,26 +49,36 @@ io.on('connection', function(socket){
     console.log(msg);
     io.emit('connection status', (sockets[socket.id].nick || socket.id) + " disconnected!");
   });
- 
+
   socket.on('who', function(){
    io.clients(function(error, clients){
-    var list = (sockets[clients[0]].nick || clients[0]); 
+    var list = (sockets[clients[0]].nick || clients[0]);
     for(var i=1; i<clients.length;i++){
        list = list + ", " + (sockets[clients[i]].nick || clients[i])
     }
-    socket.emit('chat message', list); 
-   }); 
+    socket.emit('chat message', list);
+   });
   });
 
   socket.on('nick', function(nick){
     // Get the old nick - default to socket.id
     var oldNick = sockets[socket.id].nick || socket.id;
-    
+
     // Save the new nick on our global sockets table
     sockets[socket.id].nick = nick;
-    io.emit('chat message',oldNick + " changed name to " + nick);    
+    io.emit('chat message',oldNick + " changed name to " + nick);
   });
-  
+
+  socket.on('typing', function(id, nick, start){
+    if(sockets[id].typing == false && start){
+      sockets[id].typing = true;
+      socket.broadcast.emit('typing', socket.id, (sockets[socket.id].nick || socket.id), true);
+    }else if(start == false){
+      sockets[id].typing = false;
+      socket.broadcast.emit('typing', socket.id, (sockets[socket.id].nick || socket.id), false);
+    }
+  });
+
 });
 
 http.listen(3000, function(){
